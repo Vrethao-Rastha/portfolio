e06db7108abbb5383de8905c220d7ab6743e9f10
BryanCox <bryanc71@yahoo.com>
2018-10-17 10:15:24 -0700

    done?

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MetaTags from 'react-meta-tags';
 import Landing from './Components/Landing'
 import './index.css'
 
@@ -6,6 +7,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <MetaTags>
+        <meta property="og:title" content="Portfolio" />
+        <meta property="og:image" content="src/images/apocalypse-483425_1920.jpg" />
+        </MetaTags>
+
         <Landing />
       </div>
     );
