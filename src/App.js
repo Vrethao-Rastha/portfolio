93b35bfa61a13d704d898ed5e93f6ae80efaced7
BryanCox <bryanc71@yahoo.com>
2018-10-17 11:12:38 -0700

    done?

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import MetaTags from 'react-meta-tags';
 import Landing from './Components/Landing'
 import './index.css'
 
@@ -7,12 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <MetaTags>
-        <meta property="og:title" content="Portfolio" />
-        <meta property="og:image" content="src/images/apocalypse-483425_1920.jpg" />
-        </MetaTags>
-
+        
         <Landing />
+        
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { hydrate, render } from "react-dom";
 import "jquery/dist/jquery.min.js";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -8,4 +9,10 @@ import { faCoffee, faWrench, faUserNinja, faBook ,faFile, faAddressBook, faEnvel
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import App from './App';
 library.add(fab, faCoffee, faWrench, faUserNinja, faBook ,faFile, faAddressBook, faEnvelopeSquare, faGithubSquare, faLinkedin, faBoxOpen, faLaptop);
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
